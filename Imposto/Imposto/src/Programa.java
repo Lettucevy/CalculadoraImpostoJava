1e0495a17ddcb5c119863e23d3b3e4b2d06518ef
Vitório Gabriel <lettuce3200@gmail.com>
2024-10-04 13:43:01 -0300

    Update Programa.java

--- Imposto/Imposto/src/Programa.java
+++ Imposto/Imposto/src/Programa.java
@@ -4,7 +4,6 @@ public class Programa {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Leitura dos dados fornecidos pelo usuário
         System.out.print("Renda anual com salário: ");
         double rendaSalarioAnual = scanner.nextDouble();
 
@@ -20,10 +19,8 @@ public class Programa {
         System.out.print("Gastos educacionais: ");
         double gastosEducacionais = scanner.nextDouble();
 
-        // Criação de objeto Pessoa com os dados fornecidos
         Imposto imposto = new Imposto(rendaSalarioAnual, rendaServicosAnual, rendaCapitalAnual, gastosMedicos, gastosEducacionais);
 
-        // Cálculo do imposto usando a classe CalculadoraImposto
         CalculadoraImposto calculadora = new CalculadoraImposto(imposto);
         calculadora.gerarRelatorio();
 
